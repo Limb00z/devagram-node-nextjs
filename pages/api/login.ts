018f3ed26cd9f0261003ad5fe9ee38ccc31be498
Ruan Reis <86503203+Limb00z@users.noreply.github.com>
2022-11-16 10:57:48 -0300

    mudando a senha do teste de api

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -7,7 +7,7 @@ export default(
     if(req.method === 'POST'){
         const{login,senha} = req.body;
 
-        if(login === 'admin@admin.com' && senha === 'admin123'){
+        if(login === 'admin@admin.com' && senha === 'Admin123'){
             return res.status(200).json({msg : 'Usuario autenticado com sucesso'});
         }
         return res.status(400).json({erro: 'Usuario nao encontrado'});
