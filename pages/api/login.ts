43e8375c9b79bff943ef3f84b8cb21331d60750f
Ruan Reis <86503203+Limb00z@users.noreply.github.com>
2022-11-18 23:51:04 -0300

    Criando uma const para encontrar usuarios

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,21 @@
 import type { NextApiRequest,NextApiResponse } from "next";
 import { conectarMongoDb } from "../../middlewares/conectaMongoDB";
 import type { respostaPadraoMsg } from "../../types/respostaPadraoMsg";
+import md5 from 'md5';
+import { UsuarioModel } from "../../models/UsuarioModel";
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest,
     res : NextApiResponse<respostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const{login,senha} = req.body;
 
-        if(login === 'admin@admin.com' && senha === 'Admin123'){
-            return res.status(200).json({msg : 'Usuario autenticado com sucesso'});
+        const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
+
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0];
+            return res.status(200).json({msg : `Usuario ${usuarioEncontrado.nome} encontrado com sucesso.`});
         }
         return res.status(400).json({erro: 'Usuario nao encontrado'});
     }
